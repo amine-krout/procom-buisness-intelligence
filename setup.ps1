3ae319718f79acd3cd6988bc80f2424d8abab5bc
amine-krout <mohamedamine.krout@supcom.tn>
2024-02-19 14:33:23 +0100

    change the template to setup.json

--- setup.ps1
+++ setup.ps1
@@ -138,3 +138,13 @@ $sqlDatabaseName = "sql$suffix"
 
 write-host "Creating $synapseWorkspace Synapse Analytics workspace in $resourceGroupName resource group..."
 write-host "(This may take some time!)"
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+  -TemplateFile "setup.json" `
+  -Mode Complete `
+  -workspaceName $synapseWorkspace `
+  -dataLakeAccountName $dataLakeAccountName `
+  -sqlUser $sqlUser `
+  -sqlPassword $sqlPassword `
+  -uniqueSuffix $suffix `
+  -Force
